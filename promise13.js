c8cb218a4e4ea48bc4a7f1a4b4fc4fb29c5c9855
Erik Zephyr <erikbv99421>
2018-11-08 23:40:55 +0000

    exercise 14

--- promise13.js
+++ promise13.js
@@ -1,11 +1,16 @@
 const request = require('superagent')
 
-request.get('http://www.google.com/')
+function checkStatusWebsites() {
+  request.get('http://www.google.com/')
   .then((res)=> {
-    return Promise.all([res, request.get('http://www.bing.com/')])
+    return Promise.all([res, request.get('http://www.bing.com/'), request.get('http://www.kaiju-tv.com/')])
   })
-  .then(([resGoogle, resBing])=> {
-    console.log('Bing : ', resBing.type)
-    console.log('Google :', resGoogle.type)
+  .then(([resGoogle, resBing, resFB])=> {
+    console.log('Bing : ', resBing.status)
+    console.log('Google :', resGoogle.status)
+    console.log('FB :', resFB.status)
   })
   .catch(err => console.error(err))
+}
+
+checkStatusWebsites()
